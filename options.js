491eabf637441c73175c5bd5c0ba602a76b7c1b4
Dan Stuart <dstuart@barracuda.com>
2014-12-05 16:39:41 -0500

    Implemented loading data from storage and building table. Implemented clear button. Implemented saving profile data from table.

--- options.js
+++ options.js
@@ -17,6 +17,37 @@ $.fn.serializeObject = function()
    return o;
 };
 
+numProfiles = 0;
+
+function addProfileRow(data)
+{
+	var rowHTML = '<tr id="profile' + numProfiles +'">' + 
+		'<td><input type="text" class="domain" name="domain"></td>' + 
+		'<td>' + 
+		  '<select class="mode" name="mode" class="mode">' + 
+		    '<option value="all">All</option>' + 
+		    '<option value="individual">Individual</option>' + 
+		    '<option value="disabled">Disabled</option>' + 
+		  '</select>' + 
+		'</td>' + 
+		'<td><input type="text" class="section_selector" name="section_selector"></td>' + 
+		'<td><input type="text" class="comment_selector" name="comment_selector"></td>' + 
+	'</tr>';
+
+	$("table#profiles > tbody").append(rowHTML);
+
+	if (typeof data !== 'undefined') {
+		var row = $('tr#profile' + numProfiles);
+		var fields = ["domain", "mode", "section_selector", "comment_selector"];
+		
+		for (f of fields) {
+			row.find("." + f).val(data[f]);
+		}
+	}
+
+	numProfiles++;
+}
+
 $(document).ready(function() {
 	// Load options
 	chrome.storage.sync.get("awesome_data", function(data) {
@@ -25,9 +56,36 @@ $(document).ready(function() {
 		console.log(data);
 	});
 
+	// Load up saved profiles
+	chrome.storage.sync.get("profiles", function(data) {
+		console.log(data);
+		var profiles = data["profiles"];
+
+		for (p of profiles) {
+			addProfileRow(p);
+		}
+	});
+
+	// Add profile button
+	$("#add_profile").on('click', function() {
+		addProfileRow();
+	});
+
 	// Save options
 	$("#save").on('click', function() {
-		data = $("#options_form").serializeObject();
+		data = {"awesome_data": $("#awesome_data").val(),
+				"profiles": []};
+
+		$("table#profiles > tbody").find("tr").each(function(ind, row) {
+			var profile = {};
+			var fields = ["domain", "mode", "section_selector", "comment_selector"];
+			
+			for (f of fields) {
+				profile[f] = $(row).find("." + f).val();
+			}
+			data.profiles.push(profile);
+		});
+
 		console.log(data);
 
 		chrome.storage.sync.set(data, function() {
@@ -37,10 +95,9 @@ $(document).ready(function() {
 
 	// Clear options
 	$("#clear").on('click', function() {
-		chrome.storage.sync.clear(function() {
-			console.log("Cleared!");
-			location.reload();
-		});
+		loadStartingData();
+		console.log("Cleared!");
+		location.reload();
 	});
 });
 
