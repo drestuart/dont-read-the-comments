c537df44f690e001237ab52b4da4358c6aa7562e
Dan Stuart <dstuart@barracuda.com>
2014-12-05 16:38:09 -0500

    Added some default data to install script

--- install.js
+++ install.js
@@ -1,5 +1,15 @@
 function loadStartingData() {
-	chrome.storage.sync.set({"awesome_data" : "awesome by default"}, 
+	var fresh_data = {"awesome_data" : "awesome by default"};
+
+	// Load up default website profiles
+	var profiles = [
+		{"domain": "youtube.com", "mode": "all", "section_selector": "#comments", "comment_selector": ""},
+		{"domain": "", "mode": "all", "section_selector": "#comments", "comment_selector": ""}
+	];
+
+	fresh_data.profiles = profiles;
+
+	chrome.storage.sync.set(fresh_data, 
     function() {
             console.log("Installed fresh data");
     });
